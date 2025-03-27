c936149c356c9f94e6b206f7c7b564fa95e65ace
Nilton <niltondutra@me.com>
2025-03-27 12:25:16 +0100

    Coponents

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ProfileCardComponent } from './components/profile-card/profile-card.component';
+import { FriendsListComponent } from './components/friends-list/friends-list.component';
+import { TabsComponent } from './components/tabs/tabs.component';
+import { MapComponent } from './components/map/map.component';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    ProfileCardComponent,
+    FriendsListComponent,
+    TabsComponent,
+    MapComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private baseUrl = 'http://randomuser.me/api/'
+
+  constructor(private http: HttpClient) { }
+
+  getUser(): Observable<any> {
+    return this.http.get(this.baseUrl);
+  }
+
+  getUsersList(count: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}?results=${count}`);
+  }
+}
